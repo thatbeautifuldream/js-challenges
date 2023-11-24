82656204c87d3a66af7d8caaa23a2c4c3d32e283
Milind Mishra ▲ <milind.mishra4@gmail.com>
2023-11-24 16:39:46 +0530

    📦 NEW: find max number

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,19 @@
-function findMaxNumber() {}
+function findMaxNumberBuiltIn(arr) {
+  // Math.max() takes a list of numbers as arguments and spits out the largest number
+  // ...arr spreads the array into a list of arguments
+  // example: Math.max(1, 2, 3) => 3
+  return Math.max(...arr);
+}
+
+function findMaxNumber(arr) {
+  // Keeping a temp max variable for comparing it to each element in the array
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    // Inside the loop check if the current element is greater than the max
+    // if yes then set max to the current element
+    if (arr[i] > max) max = arr[i];
+  }
+  return max;
+}
 
 module.exports = findMaxNumber;
