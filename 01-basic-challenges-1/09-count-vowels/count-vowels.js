9c15965d9ba41346087415a626be56c223e33463
Milind Mishra ▲ <milind.mishra4@gmail.com>
2023-11-25 22:15:39 +0530

    📦 NEW: count vowels

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,17 @@
-function countVowels() {}
+function countVowels(str) {
+  // Format the string to lowercase
+  const formattedStr = str.toLowerCase();
+  // Create an array of vowels
+  const vowels = ["a", "e", "i", "o", "u"];
+  // Count variable for the occurance of each vowel
+  let count = 0;
+  for (let i = 0; i < formattedStr.length; i++) {
+    const char = formattedStr[i];
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = countVowels;
