5f585e84d6a47fecc78e31dc6b71582b566a50c9
Milind Mishra ▲ <milind.mishra4@gmail.com>
2023-11-25 22:17:37 +0530

    📦 NEW: remove duplicacy

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,14 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+  // Array to store unique values
+  const uniqueArr = [];
+  // Loop through the array to check if the value is already in the uniqueArr
+  for (let i = 0; i < arr.length; i++) {
+    if (!uniqueArr.includes(arr[i])) {
+      uniqueArr.push(arr[i]);
+    }
+  }
+
+  return uniqueArr;
+}
 
 module.exports = removeDuplicates;
