137e3becc447f450e02ed6e079bd5717f07d690a
Milind Mishra ▲ <milind.mishra4@gmail.com>
2023-11-25 22:05:41 +0530

    📦 NEW: reverse string

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,18 @@
-function reverseString() {}
+function reverseStringLoop(str) {
+  // Store the initial reverse string
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    // Loop through the string backwards and keep storing the characters
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+function reverseString(string) {
+  // Split the string into an array of characters
+  // Reverse the array
+  // Join the array back into a string
+  return string.split("").reverse().join("");
+}
 
 module.exports = reverseString;
